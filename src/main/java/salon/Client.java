e8434f479dfdc48286fb2f2c6d42670bc6ea0262
Antony Nganga <antonynganga05@gmail.com>
2019-04-19 20:17:58 +0300

    Adds contructors to client and stylist objects

--- src/main/java/salon/Client.java
+++ src/main/java/salon/Client.java
@@ -1,5 +1,17 @@
 package salon;
 
 public class Client{
+    private String name;
+    private String mobile;
+    private String email;
+    private int id;
+    private int stringid;
 
+    public Client(String name, String mobile, String email, int stringId){
+        this.name = name;
+        this.mobile = mobile;
+        this.email = email;
+        stringid = stringId;
+
+    }
 }
--- src/main/java/salon/Stylist.java
+++ src/main/java/salon/Stylist.java
@@ -1,4 +1,19 @@
 package salon;
+
+import java.time.LocalDateTime;
+
 public class Stylist{
+    private String name;
+    private String mobile;
+    private String email;
+    private LocalDateTime date;
+    private int id;
+
+    public Stylist(String name, String mobile, String email){
+       this.name = name;
+       this.mobile = mobile;
+       this.email = email;
+       date = LocalDateTime.now();
+    }
 
 }
