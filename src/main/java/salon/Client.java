90012af59d4f7634e2bd3bcde890bcc2d1563a31
Antony Nganga <antonynganga05@gmail.com>
2019-04-20 16:42:49 +0300

    Adds tests for save and retrieve from database

--- src/main/java/salon/Client.java
+++ src/main/java/salon/Client.java
@@ -1,17 +1,20 @@
 package salon;
 
+import org.sql2o.*;
+import java.util.List;
+
 public class Client{
     private String name;
     private String mobile;
     private String email;
     private int id;
-    private int stringid;
+    private int stylist_id;
 
-    public Client(String name, String mobile, String email, int stringId){
+    public Client(String name, String mobile, String email, int stylistId){
         this.name = name;
         this.mobile = mobile;
         this.email = email;
-        stringid = stringId;
+        this.stylist_id = stylistId;
     }
 
     public String getName(){
@@ -22,5 +25,37 @@ public class Client{
     }
     public String getMobile(){
         return mobile;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public static List<Client> clientsAll(){
+       String sql ="SELECT * FROM clients;";
+        try(Connection connect = DB.sql2o.open()) {
+        return connect.createQuery(sql).executeAndFetch(Client.class);
+       }
+    }
+    @Override
+    public boolean equals(Object testClient){
+        if(!(testClient instanceof Client)){
+            return false;
+        }
+        else{
+            Client newClient = (Client) testClient;
+            return this.clientsAll().equals(newClient.clientsAll());
+        }
+    }
+    public void save(){
+        try(Connection connect = DB.sql2o.open()){
+            String sql = "INSERT INTO clients(name, mobile, email, stylist_id) VALUES (:name, :mobile, :email, :stylist_id);";
+            this.id = (int) connect.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("mobile", this.email)
+                    .addParameter("email", this.email)
+                    .addParameter("stylist_id", this.stylist_id)
+                    .executeUpdate()
+                    .getKey();
+        }
     }
 }
--- src/test/java/salon/ClientTest.java
+++ src/test/java/salon/ClientTest.java
@@ -21,18 +21,52 @@ public class ClientTest{
     assertEquals(true, testClient instanceof Client);
     }
     @Test
-    public void Client_instantiatesWithClientName_String(){
+    public void getName_instantiatesWithClientName_String(){
         Client testClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
         assertEquals("Anne O.", testClient.getName());
     }
     @Test
-    public void Client_instantiatesWithEmail_String(){
+    public void getEmail_instantiatesWithEmail_String(){
         Client testClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
         assertEquals("anne0@gmail.com", testClient.getEmail());
     }
     @Test
-    public void Client_instantiatesWithClientMobile_String(){
+    public void getMobile_instantiatesWithClientMobile_String(){
         Client testClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
         assertEquals("0745-456-673", testClient.getMobile());
+    }
+    @Test
+    public void getId_clientInstantiatesWithIdAttribute_int(){
+        Client testClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
+        testClient.save();
+        assertTrue(testClient.getId()>0);
+    }
+    @Test
+    public void clientsAll_returnsAllTaskObjectsFromDatabase_true(){
+        Client firstClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
+        firstClient.save();
+        Client secondClient = new Client("Yvone", "0732-786-932", "yvone@gmail.com", 2);
+        secondClient.save();
+        assertEquals(true, Client.clientsAll().get(0).equals(firstClient));
+        assertEquals(true, Client.clientsAll().get(1).equals(secondClient));
+    }
+    @Test
+    public  void equals_returnsTrueIfClientObjectsAreSame(){
+        Client firstClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
+        Client secondClient = new Client("Yvone", "0732-786-932", "yvone@gmail.com", 2);
+        assertTrue(firstClient.equals(secondClient));
+    }
+    @Test
+    public void save_returnsTrueIfClientObjectsAreSame(){
+        Client testClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
+        testClient.save();
+        assertTrue(Client.clientsAll().get(0).equals(testClient));
+    }
+    @Test
+    public void save_assignsUniqueIdToOjectsSavedInDatabase(){
+        Client testClient = new Client("Anne O.", "0745-456-673", "anne0@gmail.com", 1);
+        testClient.save();
+        Client savedClient = Client.clientsAll().get(0);
+        assertEquals(testClient.getId(), savedClient.getId());
     }
 }
